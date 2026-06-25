19236d0cb357f44d7f763c6706f4f391458d41cd
SienZz <okp1988@hotmail.com>
2026-06-25 10:49:44 +0800

    Expand Y-axis offset options in OffsetDisplayOption
    Added new Y-axis offset steps (-6 to -4 and 4 to 6) to OffsetDisplayOption.All, allowing finer control for both upward and downward offsets.

--- OffsetDisplayOption.cs
+++ OffsetDisplayOption.cs
@@ -7,12 +7,18 @@ namespace Lazy_App_Codex_Core
         public static readonly OffsetDisplayOption[] All =
         {
             new("0", "No offset"),
+            new("-6:y", "Y: Up 6 steps"),
+            new("-5:y", "Y: Up 5 steps"),
+            new("-4:y", "Y: Up 4 steps"),
             new("-3:y", "Y: Up 3 steps"),
             new("-2:y", "Y: Up 2 steps"),
             new("-1:y", "Y: Up 1 step"),
             new("1:y", "Y: Down 1 step"),
             new("2:y", "Y: Down 2 steps"),
             new("3:y", "Y: Down 3 steps"),
+            new("4:y", "Y: Down 4 steps"),
+            new("5:y", "Y: Down 5 steps"),
+            new("6:y", "Y: Down 6 steps"),
             new("-3:x", "X: Left 3 steps"),
             new("-2:x", "X: Left 2 steps"),
             new("-1:x", "X: Left 1 step"),
